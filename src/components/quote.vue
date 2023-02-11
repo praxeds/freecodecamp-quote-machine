ed5c81cef9075f85741f5e881e1934991da4596b
Juliana Praxedes <julianapraxeds@gmail.com>
2023-02-11 03:27:07 -0300

    Updated API data styling

--- src/components/quote.vue
+++ src/components/quote.vue
@@ -1,8 +1,9 @@
 <template>
   <section>
-    <h1>{{ proverb }}</h1>
-    <h2>{{ pinyin }}</h2>
-    <h3>{{ translation }}</h3>
+    <h2 :class="{ fadeout: disabled }">{{ pinyin }}</h2>
+    <h1 :class="{ fadeout: disabled }">{{ proverb }}</h1>
+    <h3 :class="{ fadeout: disabled }">{{ translation }}</h3>
+    <button @click="fetchNewProverb">Click me</button>
   </section>
 </template>
 
@@ -13,11 +14,14 @@ export default {
   data() {
     return {
       proverb: null,
+      pinyin: null,
+      translation: null,
+      disabled: false,
     };
   },
   methods: {
     async fetchProverb() {
-      axios
+      await axios
         .get("https://chinese-proverbs.onrender.com/api/proverbs/random")
         .then(
           (response) => (
@@ -27,6 +31,13 @@ export default {
           )
         );
     },
+    fetchNewProverb() {
+      this.disabled = true;
+      this.fetchProverb();
+      setTimeout(() => {
+        this.disabled = false;
+      }, 2000);
+    },
   },
   beforeMount() {
     this.fetchProverb();
@@ -35,6 +46,9 @@ export default {
 </script>
 
 <style scoped lang="scss">
+// Colors
+$medium-gray: #b6b5b6;
+
 section {
   height: inherit;
   width: inherit;
@@ -47,26 +61,69 @@ section {
   justify-content: center;
   align-items: center;
 
-  h1,h2,h3 {
+  > * {
     margin: 0;
+    text-align: center;
+  }
+
+  h1 {
+    font-size: 4rem;
+    font-weight: 400;
+    margin-bottom: 0.7rem;
+    &::before {
+      content: "“";
+      font-size: 6rem;
+      font-weight: 700;
+      margin-right: 0.5rem;
+    }
+  }
+
+  h2 {
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: inherit;
+    font-size: 1rem;
+    font-weight: 500;
+    word-spacing: 2.2rem;
   }
 
   button {
-    margin-left: 1rem;
-    padding: 0.5rem 1rem;
+    margin: 0;
+    padding: 1.5rem 4.5rem;
     border: none;
-    border-radius: 0.5rem;
-    background: #f1f1f1;
-    color: #333;
-    font-size: 1rem;
-    font-weight: 600;
-    cursor: pointer;
-    transition: all 0.2s ease-in-out;
+    border-radius: 0.3rem;
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #fdfdfd;
+    box-shadow: 0 2px 1px 0 $medium-gray;
 
     &:hover {
       background: #333;
       color: #f1f1f1;
     }
+  }
+}
+
+.fadeout {
+  animation: fadeout 2s ease;
+}
+
+@keyframes fadeout {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
   }
 }
 </style>
